030306c905553627f3485e8121b51ee83b48799b
Ted Szylowiec <tedszy@gmail.com>
2016-08-12 15:14:42 +0700

    fixed exports, test return nil

--- cl-pentothal.lisp
+++ cl-pentothal.lisp
@@ -5,21 +5,23 @@
 (defparameter *results* nil)
 
 (defmacro test (name function-form comparison expected)
-  `(push (list ',name 
-	       (lambda () 
-		 (if (funcall #',comparison
-			      ,expected
-			      ,function-form)
-		     (progn (incf *passed*)
-			    (format t "ok: ~a~%" ',name))
-		     (progn (incf *failed*)
-			    (format t
-				    "NOT OK: ~a ==> got ~a, expected ~a in ~a~%"
-				    ',name
-				    ,function-form
-				    ,expected
-				    ',function-form)))))
-	 *results*))
+  `(progn 
+     (push (list ',name 
+		 (lambda () 
+		   (if (funcall #',comparison
+				,expected
+				,function-form)
+		       (progn (incf *passed*)
+			      (format t "ok: ~a~%" ',name))
+		       (progn (incf *failed*)
+			      (format t
+				      "NOT OK: ~a ==> got ~a, expected ~a in ~a~%"
+				      ',name
+				      ,function-form
+				      ,expected
+				      ',function-form)))))
+	   *results*)
+     nil))
 
 (defun run-tests ()
   (setf *passed* 0)
--- package.lisp
+++ package.lisp
@@ -1,6 +1,4 @@
 (defpackage #:cl-pentothal
   (:use #:cl)
-  (:export #:test-me
-	   #:run-tests
-	   #:*passed*
-	   #:*failed*))
+  (:export #:test
+	   #:run-tests))
